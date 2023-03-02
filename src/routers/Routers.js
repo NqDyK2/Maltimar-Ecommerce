48703e012d8444be21abc76992c95dd51bf8e876
Duy. Nguyen Quy - CMC Global DJ2 <nguyenquyduyabc@gmail.com>
2023-03-02 14:52:24 +0700

    Feat: UI Header

---
+++ src/routers/Routers.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Route, Routes, Navigate } from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Cart from '../pages/Cart'
+import Checkout from '../pages/Checkout'
+import Login from '../pages/Login'
+import ProductDetail from '../pages/ProductDetail'
+import Signup from '../pages/Signup'
+import Shop from '../pages/Shop'
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigate to='home' />}/>
+      <Route path='home' element={<Home />}/>
+      <Route path='shop' element={<Shop />}/>
+      <Route path='shop/:id' element={<ProductDetail />}/>
+      <Route path='cart' element={<Cart />}/>
+      <Route path='checkout' element={<Checkout />}/>
+      <Route path='login' element={<Login />}/>
+      <Route path='signup' element={<Signup />}/>
+    </Routes>
+  )
+}
+
+export default Routers
