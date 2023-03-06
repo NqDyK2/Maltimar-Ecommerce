d286cd0050627bc51fa1788f870cb6cd40947856
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-03-06 23:36:59 +0700

    Feat:Update done UI component Header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion' 
 import { Col, Container, Row } from 'reactstrap'
 import "../styles/home.css"
 import Helmet from '../components/Helmet/Helmet'
 import heroImg from "../assets/images/hero-img.png"
 
-const Home = () => {
-  const year = new Date() . getFullYear()
+const Home = () => {  
+  
+  const year = new Date().getFullYear()
+
   return <Helmet title={"Home"}>
     <section className='hero__section'>
       <Container>
@@ -20,7 +24,7 @@ const Home = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
               </p>
 
-              <button className="buy_btn">SHOP NOW</button>
+              <motion.button whileTap={{ scale:1.2 }} className="buy__btn"><Link to="/shop">SHOP NOW</Link></motion.button>
             </div>
           </Col>
 
