8e11e00e838933efd98cf32247687fa51b253f7b
Duy. Nguyen Quy - CMC Global DJ2 <nguyenquyduyabc@gmail.com>
2023-03-08 11:51:06 +0700

    Feat: Responsive menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React,{useRef, useEffect} from 'react'
+import React, { useRef, useEffect } from 'react'
 
 import { NavLink } from 'react-router-dom'
 import './header.css'
@@ -26,7 +26,29 @@ const nav__link = [
 ]
 
 const Header = () => {
-  return <header className='header'>
+
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+
+  const stickyHeaderFunc = () => {
+    window.addEventListener('scroll', () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header')
+      } else {
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(() => {
+    stickyHeaderFunc()
+
+    return () => window.removeEventListener("scroll", stickyHeaderFunc)
+  })
+
+  const menuToggle = () => menuRef.current.classList.toggle('active__menu')
+
+  return <header className='header' ref={headerRef}>
     <Container>
       <Row>
         <div className="nav__wrapper">
@@ -38,7 +60,7 @@ const Header = () => {
             </div>
           </div>
 
-          <div className='navigation'>
+          <div className='navigation' ref={menuRef} onClick={menuToggle}>
             <ul className="menu">
               {
                 nav__link.map((item, index) => (
@@ -66,16 +88,14 @@ const Header = () => {
               <i className="ri-shopping-bag-line"></i>
               <span className='badge'>1</span>
             </span>
-            
+
             <span><motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" /></span>
+            <div className="mobile__menu">
+              <span onClick={menuToggle}>
+                <i className="ri-menu-line"></i>
+              </span>
+            </div>
           </div>
-
-          <div className="mobile__menu">
-            <span>
-              <i className="ri-menu-line"></i>
-            </span>
-          </div>
-
         </div>
       </Row>
     </Container>
