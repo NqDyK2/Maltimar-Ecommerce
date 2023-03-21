bb28052b65c46b37a96416cf02f71adfc8acbf49
Duy. Nguyen Quy - CMC Global DJ2 <nguyenquyduyabc@gmail.com>
2023-03-21 16:46:15 +0700

    Feat: Signup and upload avatar to firebase

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore'
+import {getStorage} from 'firebase/storage'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBpzyJqf5Y9V_tkJfp4eW53fv2Hwtb-IbY",
   authDomain: "reduxecommerce-cd638.firebaseapp.com",
@@ -13,8 +12,9 @@ const firebaseConfig = {
   appId: "1:491682055070:web:90763880bebae6a3f40c8f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
 
 export default app
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react';
 import Helmet from '../components/Helmet/Helmet';
 import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import { setDoc, doc } from 'firebase/firestore';
+import { auth } from '../firebase.config'
+import { storage } from '../firebase.config';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify'
 import "../styles/login.css"
 
 const Signup = () => {
@@ -11,6 +17,45 @@ const Signup = () => {
   const [password, setPassword] = useState('')
   const [username, setUsername] = useState('')
   const [file, setFile] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  const signup = async (e) => {
+    e.preventDefault()
+    setLoading(true);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+      const storageRef = ref(storage, `images/${Date.now() + username}`)
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      uploadTask.on((error) => {
+        toast.error(error.message)
+      }, () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          //Update avt user profile
+          await updateProfile(user, {
+            displayName: username,
+            photoURL: downloadURL,
+          });
+
+          //store user data in firestore database 
+          await setDoc(doc(db, 'users', user.uid), {
+            uid: user.uid,
+            displayName: username,
+            email,
+            photoURL: downloadURL,
+          })
+
+        });
+      });
+
+      console.log(user);
+    } catch (err) {
+      toast.error('Something went wrong')
+    }
+
+  }
 
   return <Helmet title='Registation'>
     <section>
@@ -19,9 +64,9 @@ const Signup = () => {
           <Col lg='6' className='m-auto text-center'>
             <h3 className="fw-bold mb-4"> Register </h3>
 
-            <Form className='auth__form'>
+            <Form className='auth__form' onSubmit={signup}>
               <FormGroup className='form__group'>
-                <input type="text" placeholder='Enter your email' value={username} onChange={e => setUsername(e.target.value)} />
+                <input type="text" placeholder='Enter your name' value={username} onChange={e => setUsername(e.target.value)} />
               </FormGroup>
 
               <FormGroup className='form__group'>
