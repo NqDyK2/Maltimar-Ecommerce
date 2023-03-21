50fc9cc3cdca9d445101ad3d8a1a93eb4d9733fa
Duy. Nguyen Quy - CMC Global DJ2 <nguyenquyduyabc@gmail.com>
2023-03-21 09:59:06 +0700

    Update component checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap'
+import Helmet from '../components/Helmet/Helmet'
+import CommoSection from '../components/UI/CommoSection'
+import "../styles/checkout.css"
 
 const Checkout = () => {
-  return (
-    <div>Checkout</div>
-  )
+
+  const totalQty = useSelector(state => state.cart.totalQuantity)
+  const totalAmount = useSelector(state => state.cart.totalAmount)
+
+  return <Helmet title="Checkout">
+    <CommoSection title="Checkout" />
+    <section>
+      <Container>
+        <Row>
+          <Col lg='8'>
+            <h6 className='mb-4 fw-bold'>Billing Information</h6>
+            <Form>
+              <FormGroup className='form__group'>
+                <input type="text" placeholder='Enter your name' />
+              </FormGroup>
+
+              <FormGroup className='form__group'>
+                <input type="email" placeholder='Enter your mail' />
+              </FormGroup>
+
+              <FormGroup className='form__group'>
+                <input type="number" placeholder='Phone number' />
+              </FormGroup>
+
+              <FormGroup className='form__group'>
+                <input type="text" placeholder='Street address' />
+              </FormGroup>
+
+              <FormGroup className='form__group'>
+                <input type="text" placeholder='City' />
+              </FormGroup>
+
+              <FormGroup className='form__group'>
+                <input type="text" placeholder='Postal code' />
+              </FormGroup>
+
+              <FormGroup className='form__group'>
+                <input type="text" placeholder='Country' />
+              </FormGroup>
+            </Form>
+          </Col>
+
+          <Col lg='4'>
+            <div className="checkout__cart">
+              <h6>
+                Total Qty: <span>{totalQty} items</span>
+              </h6>
+              <h6>
+                Subtotal: <span>${totalAmount}</span>
+              </h6>
+              <h6>
+                Shipping: <br /> free shipping <span>$0</span>
+              </h6>
+              <h4>
+                Total Cost: <span>${totalAmount}</span>
+              </h4>
+              <button className="buy__btn auth__btn w-100 checkout-btn">
+                Place an order
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  </Helmet>
 }
 
 export default Checkout
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  return <Helmet>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6' className='m-auto text-center'>
+            <h3 className="fw-bold fs-5"> Login </h3>
+
+            <Form>
+              <FormGroup className='form__group'> 
+                <input type="email" placeholder='Enter your email'/>
+              </FormGroup>
+
+              <FormGroup className='form__group'> 
+                <input type="password" placeholder='Enter your password'/>
+              </FormGroup>
+
+              <button className="buy__btn auth__btn">
+                Login
+              </button>
+              <p>Don't have an account <Link to='/signup'>Create an account</Link></p>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    </Helmet>
 }
 
 export default Login
