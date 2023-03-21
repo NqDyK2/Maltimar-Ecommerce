6c17144d2ef1c8aeedc0933a60426ff7d096098f
Duy. Nguyen Quy - CMC Global DJ2 <nguyenquyduyabc@gmail.com>
2023-03-21 11:37:12 +0700

    Feat: update firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpzyJqf5Y9V_tkJfp4eW53fv2Hwtb-IbY",
+  authDomain: "reduxecommerce-cd638.firebaseapp.com",
+  projectId: "reduxecommerce-cd638",
+  storageBucket: "reduxecommerce-cd638.appspot.com",
+  messagingSenderId: "491682055070",
+  appId: "1:491682055070:web:90763880bebae6a3f40c8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app
